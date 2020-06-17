e9f3654b4231541ee01f0433466d60152f6f6049
shanon <13fritsh@gmail.com>
2020-06-17 16:12:42 -0500

    added my initial values

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  termsofuse: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  termsofuse: false,
+}
+
+const initialMembers = []
+
+const initialDisabled = true
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>Please Sign-Up!</h1></header>
+      <div>
+        <div>Form</div>
+        <div>Card</div>
+      </div>
     </div>
   );
 }
