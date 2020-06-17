931decbc9f691a54fde15ed89a5e430dda25d8d6
shanon <13fritsh@gmail.com>
2020-06-17 17:14:38 -0500

    added my new member helper

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -39,6 +39,19 @@ function App() {
     })
   }
 
+  const newMember = () =>{
+    axios.post('https://reqres.in/api/users')
+    .then(response =>{
+      setMembers([...members, response.data.data])
+    })
+    .catch(error =>{
+      console.log('this is the error:', error)
+    })
+    .finally(() =>{
+      setFormValues(initialFormValues)
+    })
+  }
+
   return (
     <div className="App">
       <header><h1>Please Sign-Up!</h1></header>
