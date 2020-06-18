245df7f2a5f8f14a8756681dc31283d4c6d55bcf
shanon <13fritsh@gmail.com>
2020-06-18 16:20:42 -0500

    added test for first name, last name, and submit button

--- sign-up/cypress/integration/form_test.js
+++ sign-up/cypress/integration/form_test.js
@@ -1,9 +1,31 @@
-describe("test input and submit button", () =>{
+describe("test first name input and submit button", () =>{
     it('can navigate to the site', ()=>{
         cy.visit('http://localhost:3000/')
     })
 
     it('submit button should be disabled', () =>{
+        cy.get('button').should('be.disabled')
+    })
+
+    it('can type new first name', () =>{
+        cy.get('input[name=firstName]')
+        .type('Shanon')
+        .should('have.value', 'Shanon')
+    })
+
+    it('sumbit button should still be disabled', ()=>{
+        cy.get('button').should('be.disabled')
+    })
+})
+
+describe('test last name input and submit button', ()=>{
+    it('can type new last name', ()=>{
+        cy.get('input[name=lastName]')
+        .type('Fritz')
+        .should('have.value', 'Fritz')
+    })
+
+    it('submit button should still be disabled', ()=>{
         cy.get('button').should('be.disabled')
     })
 })
